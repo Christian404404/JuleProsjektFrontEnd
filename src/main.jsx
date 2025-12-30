4c544a3987e04f090b57939e0ddae8e9555df123
Christian Kodehode <christian404.kodehode@gmail.com>
2025-12-30 11:05:27 +0100

    added tanstack on import and render

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./tanStackQuery/queryClient.js";
 
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
